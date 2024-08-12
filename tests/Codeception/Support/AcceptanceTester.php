cf111d54b7aa3ab5455dd1b84b0cf4553bbcc82e
Nikola Ivanovski <nikola.ivanovski@dnhsoft.com>
2024-08-12 15:28:13 +0300

    OXDEV-8553 Add codeception skeleton

---
+++ tests/Codeception/Support/AcceptanceTester.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\SecurityModule\Tests\Codeception\Support;
+
+/**
+ * Inherited Methods
+ *
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
+ *
+ * @SuppressWarnings(PHPMD)
+ */
+final class AcceptanceTester extends \Codeception\Actor
+{
+    use _generated\AcceptanceTesterActions;
+
+}
---
+++ tests/Fixtures/testdata_ce.sql
@@ -0,0 +1,2 @@
+SET @@session.sql_mode = '';
+
