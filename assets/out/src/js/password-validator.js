bc910d324d042c4438faa989b8e9e46c261d0b86
Nikola Ivanovski <nikola.ivanovski@dnhsoft.com>
2024-08-30 14:53:41 +0300

    OXDEV-8570 Set progressbar width from JS

--- assets/out/src/js/password-validator.js
+++ assets/out/src/js/password-validator.js
@@ -2,49 +2,57 @@
  * Copyright © OXID eSales AG. All rights reserved.
  * See LICENSE file for license details.
  */
+let delayInMilliseconds = 1000;
+let requestDelayTimeoutId;
 
-const registerPassword = document.querySelector("input[id='userPassword']");
-if(registerPassword) {
-    registerPassword.addEventListener("keyup", ajaxRequest);
-}
+const passwords = document.querySelectorAll("input[id='userPassword'], input[id='passwordNew'], input[id='forgotPassword']");
+passwords.forEach(function (el) {
+    el.addEventListener("keyup", function (e) {
+        let password = this.value;
+        if (requestDelayTimeoutId !== undefined) {
+            clearTimeout(requestDelayTimeoutId);
+        }
 
-const passwordNew =  document.querySelector("input[id='passwordNew']");
-if(passwordNew) {
-    passwordNew.addEventListener("keyup", ajaxRequest);
-}
+        requestDelayTimeoutId = setTimeout(function () {
+            ajaxRequest(password);
+        }, delayInMilliseconds);
+    });
+});
 
-const forgotPassword =  document.querySelector("input[id='forgotPassword']");
-if(forgotPassword) {
-    forgotPassword.addEventListener("keyup", ajaxRequest);
-}
-
-//TODO: kill previous ajax call
-function ajaxRequest() {
-    var xhr = new XMLHttpRequest();
+function ajaxRequest(password) {
+    const xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
-            var jsonResponse = JSON.parse(xhr.responseText);
+            let jsonResponse = JSON.parse(xhr.responseText);
+            let progressBar = document.querySelector('.progress-bar');
 
-            var progressBar = document.querySelector('.progress-bar');
             progressBar.classList.remove('bg-danger', 'bg-warning', 'bg-success');
-            progressBar.style.width = (jsonResponse.strength * 25) + "%";
-            progressBar.parentNode.setAttribute('aria-valuenow', (jsonResponse.strength * 25));
 
             switch (jsonResponse.strength) {
                 case 0:
+                    progressBar.style.width = "15%";
+                    progressBar.parentNode.setAttribute('aria-valuenow', '15');
                     progressBar.classList.add('bg-danger')
                     break
                 case 1:
+                    progressBar.style.width = "35%";
+                    progressBar.parentNode.setAttribute('aria-valuenow', '35');
                     progressBar.classList.add('bg-danger')
                     break
                 case 2:
+                    progressBar.style.width = "50%";
+                    progressBar.parentNode.setAttribute('aria-valuenow', '50');
                     progressBar.classList.add('bg-warning')
                     break
                 case 3:
+                    progressBar.style.width = "75%";
+                    progressBar.parentNode.setAttribute('aria-valuenow', '75');
                     progressBar.classList.add('bg-success')
                     break
                 case 4:
+                    progressBar.style.width = "100%";
+                    progressBar.parentNode.setAttribute('aria-valuenow', '100');
                     progressBar.classList.add('bg-success')
                     break
                 default:
@@ -53,7 +61,7 @@ function ajaxRequest() {
         }
     };
 
-    //TODO: remove hardcode url
-    xhr.open("POST", "http://localhost.local/?cl=password&fnc=passwordStrength", true);
-    xhr.send();
+    xhr.open("POST", "http://localhost.local/?cl=password&fnc=passwordStrength", false);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send('password=' + password);
 }
