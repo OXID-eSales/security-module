92df48134f1fb9bf86286bb98a88f759e5ef4664
Tatyana Koleva <tatjana.koleva@dnhsoft.com>
2024-10-02 17:30:17 +0300

    OXDEV-8495 Add info for password min. length setting

--- views/admin_twig/de/oesecuritymodule_lang.php
+++ views/admin_twig/de/oesecuritymodule_lang.php
@@ -10,7 +10,9 @@ $sLangName = 'Deutsch';
 $aLang = [
     'SHOP_MODULE_GROUP_password_policy'                     => 'Passwort-Einstellungen',
     'SHOP_MODULE_oeSecurityPasswordMinimumLength'           => 'Minimale Länge',
-    'HELP_SHOP_MODULE_oeSecurityPasswordMinimumLength'      => 'Min. erlaubte Passwortlänge. Sollte länger als 8 sein.',
+    'HELP_SHOP_MODULE_oeSecurityPasswordMinimumLength'      => 'Min. erlaubte Passwortlänge. Sollte länger als 8 sein. ' .
+        'Sollte die in den Moduleinstellungen gewählte Länge die der Shopeinstellung für minimale Passwortlänge unterschreiten, ' .
+        'dann greift die Shopeinstellung.',
     'SHOP_MODULE_oeSecurityPasswordContainUppercase'        => 'Das Passwort muss Großbuchstaben enthalten',
     'SHOP_MODULE_oeSecurityPasswordContainLowercase'        => 'Das Passwort muss Kleinbuchstaben enthalten',
     'SHOP_MODULE_oeSecurityPasswordContainDigit'            => 'Das Passwort muss Zahlen enthalten',
--- views/admin_twig/en/oesecuritymodule_lang.php
+++ views/admin_twig/en/oesecuritymodule_lang.php
@@ -10,7 +10,9 @@ $sLangName = 'English';
 $aLang = [
     'SHOP_MODULE_GROUP_password_policy'                     => 'Password Policy',
     'SHOP_MODULE_oeSecurityPasswordMinimumLength'           => 'Minimum length',
-    'HELP_SHOP_MODULE_oeSecurityPasswordMinimumLength'      => 'Min. passwort length. Should be greater then 8.',
+    'HELP_SHOP_MODULE_oeSecurityPasswordMinimumLength'      => 'Min. password length. Should be greater than 8. ' .
+        'In case the number is less than in shop settings, ' .
+        'then the shop default minimum password length will override this value.',
     'SHOP_MODULE_oeSecurityPasswordContainUppercase'        => 'Password must contain uppercase',
     'SHOP_MODULE_oeSecurityPasswordContainLowercase'        => 'Password must contain lowercase',
     'SHOP_MODULE_oeSecurityPasswordContainDigit'            => 'Password must contain digit',
