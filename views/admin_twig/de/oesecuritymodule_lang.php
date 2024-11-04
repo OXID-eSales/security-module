f4c15bf69e20ce409ffadd010755cc7e17657f0d
Deve L. Oper <heike.reuter@oxid-esales.com>
2024-11-04 13:50:41 +0100

    Update translations

--- views/admin_twig/de/oesecuritymodule_lang.php
+++ views/admin_twig/de/oesecuritymodule_lang.php
@@ -13,8 +13,8 @@ $aLang = [
     'HELP_SHOP_MODULE_oeSecurityPasswordMinimumLength'      => 'Min. erlaubte Passwortlänge. Sollte länger als 8 sein. ' .
         'Sollte die in den Moduleinstellungen gewählte Länge die der Shopeinstellung für minimale Passwortlänge unterschreiten, ' .
         'dann greift die Shopeinstellung.',
-    'SHOP_MODULE_oeSecurityPasswordContainUppercase'        => 'Das Passwort muss Großbuchstaben enthalten',
-    'SHOP_MODULE_oeSecurityPasswordContainLowercase'        => 'Das Passwort muss Kleinbuchstaben enthalten',
-    'SHOP_MODULE_oeSecurityPasswordContainDigit'            => 'Das Passwort muss Zahlen enthalten',
-    'SHOP_MODULE_oeSecurityPasswordContainSpecialCharacter' => 'Das Passwort muss Sonderzeichen enthalten',
+    'SHOP_MODULE_oeSecurityPasswordContainUppercase'        => 'Das Passwort muss mindestens einen Großbuchstaben enthalten',
+    'SHOP_MODULE_oeSecurityPasswordContainLowercase'        => 'Das Passwort muss mindestens einen Kleinbuchstaben enthalten',
+    'SHOP_MODULE_oeSecurityPasswordContainDigit'            => 'Das Passwort muss mindestens eine Zahl enthalten',
+    'SHOP_MODULE_oeSecurityPasswordContainSpecialCharacter' => 'Das Passwort muss mindestens ein Sonderzeichen enthalten',
 ];
