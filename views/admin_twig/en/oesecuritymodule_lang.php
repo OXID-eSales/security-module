4c7acd8fca1e42749240fa5fe1c37fcc80b79f7e
Michael Keiluweit <michael.keiluweit@oxid-esales.com>
2024-11-13 11:43:47 +0100

    Gettting rid of an abbreviation in the help text

--- views/admin_twig/en/oesecuritymodule_lang.php
+++ views/admin_twig/en/oesecuritymodule_lang.php
@@ -10,10 +10,10 @@ $sLangName = 'English';
 $aLang = [
     'SHOP_MODULE_GROUP_password_policy'                     => 'Password Policy',
     'SHOP_MODULE_oeSecurityPasswordMinimumLength'           => 'Minimum length',
-    'HELP_SHOP_MODULE_oeSecurityPasswordMinimumLength'      => 'Min. password length. Should be greater than 8. ' .
+    'HELP_SHOP_MODULE_oeSecurityPasswordMinimumLength'      => 'Minimum password length. Should be greater than 8. ' .
         'In case the number is less than in shop settings, ' .
         'then the shop default minimum password length will override this value.',
-    'SHOP_MODULE_oeSecurityPasswordContainUppercase'       => 'Password must contain at least one uppercase letter',
+    'SHOP_MODULE_oeSecurityPasswordContainUppercase'        => 'Password must contain at least one uppercase letter',
     'SHOP_MODULE_oeSecurityPasswordContainLowercase'        => 'Password must contain at least one lowercase letter',
     'SHOP_MODULE_oeSecurityPasswordContainDigit'            => 'Password must contain at least one digit',
     'SHOP_MODULE_oeSecurityPasswordContainSpecialCharacter' => 'Password must contain at least one special char',
