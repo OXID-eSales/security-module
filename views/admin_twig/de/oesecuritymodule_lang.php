64fdd39a77daab1bec5fd3c73fb9e23fd61b2a5e
Michael Keiluweit <michael.keiluweit@oxid-esales.com>
2024-11-13 11:41:48 +0100

    Gettting rid of an abbreviation in the help text

--- views/admin_twig/de/oesecuritymodule_lang.php
+++ views/admin_twig/de/oesecuritymodule_lang.php
@@ -10,7 +10,7 @@ $sLangName = 'Deutsch';
 $aLang = [
     'SHOP_MODULE_GROUP_password_policy'                     => 'Passwort-Einstellungen',
     'SHOP_MODULE_oeSecurityPasswordMinimumLength'           => 'Minimale Länge',
-    'HELP_SHOP_MODULE_oeSecurityPasswordMinimumLength'      => 'Min. erlaubte Passwortlänge. Sollte länger als 8 sein. ' .
+    'HELP_SHOP_MODULE_oeSecurityPasswordMinimumLength'      => 'Mininmal erlaubte Passwortlänge. Sollte länger als 8 sein. ' .
         'Sollte die in den Moduleinstellungen gewählte Länge die der Shopeinstellung für minimale Passwortlänge unterschreiten, ' .
         'dann greift die Shopeinstellung.',
     'SHOP_MODULE_oeSecurityPasswordContainUppercase'        => 'Das Passwort muss mindestens einen Großbuchstaben enthalten',
