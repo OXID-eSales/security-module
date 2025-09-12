8bdd829961accbae072f2e2c47812b6fbbb09329
Tomasz Kowalewski <t.kowalewski@createit.pl>
2025-09-12 12:29:53 +0200

    OXDEV-8786 Remove unnecessary phpstan ignore line and refactor ExceptionFactory

--- src/PasswordPolicy/Intrastructure/ExceptionFactory.php
+++ src/PasswordPolicy/Intrastructure/ExceptionFactory.php
@@ -10,13 +10,14 @@ declare(strict_types=1);
 namespace OxidEsales\SecurityModule\PasswordPolicy\Intrastructure;
 
 use OxidEsales\Eshop\Core\Exception\InputException;
+use OxidEsales\Eshop\Core\Language;
 use OxidEsales\SecurityModule\PasswordPolicy\Validation\Exception\PasswordCollectionException;
 use OxidEsales\SecurityModule\PasswordPolicy\Validation\Exception\PasswordValidateException;
 
-class ExceptionFactory implements ExceptionFactoryInterface
+readonly class ExceptionFactory implements ExceptionFactoryInterface
 {
     public function __construct(
-        private readonly \OxidEsales\Eshop\Core\Language $language
+        private Language $language
     ) {
     }
 
@@ -26,31 +27,26 @@ class ExceptionFactory implements ExceptionFactoryInterface
             return $this->createCollection($exception);
         }
 
-        $exception = oxNew(
-            InputException::class,
-            sprintf(
-                /** @phpstan-ignore-next-line */
-                $this->language->translateString($exception->getMessage()),
-                ...$exception->getTranslationParameters()
-            )
-        );
-
-        return $exception;
+        return oxNew(InputException::class, $this->formatExceptionMessage($exception));
     }
 
     private function createCollection(PasswordCollectionException $collection): InputException
     {
         $lines = [];
-        foreach ($collection->getValidationExceptions() as $ex) {
-            $lines[] = sprintf(
-                /** @phpstan-ignore-next-line */
-                $this->language->translateString($ex->getMessage()),
-                ...$ex->getTranslationParameters()
-            );
+        foreach ($collection->getValidationExceptions() as $exception) {
+            $lines[] = $this->formatExceptionMessage($exception);
         }
-        /** @phpstan-ignore-next-line */
         $message = '<ul><li>' . implode('</li><li>', $lines) . '</li></ul>';
 
         return oxNew(InputException::class, $message);
+    }
+
+    private function formatExceptionMessage(PasswordValidateException $exception): string
+    {
+        return sprintf(
+            /** @phpstan-ignore-next-line */
+            $this->language->translateString($exception->getMessage()),
+            ...$exception->getTranslationParameters()
+        );
     }
 }
