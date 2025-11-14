2bd7ca3f79bbb01979dafd5184f47dbb508344a2
Nikola Ivanovski <nikola.ivanovski@dnhsoft.com>
2025-11-14 11:42:41 +0200

    OXDEV-9919 Adjust CC provider tests

--- tests/Codeception/Acceptance/ProvidersVisibilityCest.php
+++ tests/Codeception/Acceptance/ProvidersVisibilityCest.php
@@ -26,76 +26,85 @@ class ProvidersVisibilityCest extends BaseCest
 
     //todo: add % to link to check all providers individually
     private $providerLink =
-        '//div[contains(@class, "sign-in-providers")]' .
+        'div[contains(@class, "sign-in-providers")]' .
         '//div[@class="provider"]' .
         '//a[contains(@href, "cl=oauth&fnc=redirect&provider=")]';
 
     public function testProvidersVisibilityOnHeaderLogin(AcceptanceTester $I): void
     {
+        $providerLinkElement = '//div[contains(@class, "dropdown-menu")]//' . $this->providerLink;
+
         $homePage = $I->openShop();
         $homePage->openAccountMenu();
-        $I->dontSeeElement($this->providerLink);
+        $I->dontSeeElement($providerLinkElement);
 
         $this->setProviderState(true);
         $homePage = $I->openShop();
         $homePage->openAccountMenu();
-        $I->assertGreaterOrEquals(1, count($I->grabMultiple($this->providerLink)));
+        $I->assertGreaterOrEquals(1, count($I->grabMultiple($providerLinkElement)));
     }
 
     public function testProvidersVisibilityOnMyAccountLogin(AcceptanceTester $I): void
     {
+        $providerLinkElement = '//div[contains(@class, "card-body")]//' . $this->providerLink;
+
         $userLoginPage = new UserLogin($I);
         $I->amOnPage($userLoginPage->URL);
-        $I->dontSeeElement($this->providerLink);
+        $I->dontSeeElement($providerLinkElement);
 
         $this->setProviderState(true);
         $userLoginPage = new UserLogin($I);
         $I->amOnPage($userLoginPage->URL);
-        $I->assertGreaterOrEquals(1, count($I->grabMultiple($this->providerLink)));
+        $I->assertGreaterOrEquals(1, count($I->grabMultiple($providerLinkElement)));
     }
 
     public function testProviderVisibilityOnCheckoutPage(AcceptanceTester $I): void
     {
+        $providerLinkElement = '//div[contains(@class, "card-body")]//' . $this->providerLink;
+
         $basket = new Basket($I);
         $basket->addProductToBasketAndOpenUserCheckout('1000', 1);
-        $I->waitForElementNotVisible($this->providerLink);
+        $I->dontSeeElement($providerLinkElement);
 
         $this->setProviderState(true);
         $basket = new Basket($I);
         $basket->addProductToBasketAndOpenUserCheckout('1000', 1);
-        $I->waitForElementNotVisible($this->providerLink);
-        $I->assertGreaterOrEquals(1, count($I->grabMultiple($this->providerLink)));
+        $I->seeElement($providerLinkElement);
+        $I->assertGreaterOrEquals(1, count($I->grabMultiple($providerLinkElement)));
     }
 
     public function testProviderVisibilityOnCheckoutWithoutAccount(AcceptanceTester $I): void
     {
+        $providerLinkElement = '//div[contains(@class, "card-body")]//' . $this->providerLink;
+
         $basket = new Basket($I);
-        $basket
-            ->addProductToBasketAndOpenUserCheckout('1000', 1)
-            ->selectOptionNoRegistration();
-        $I->waitForElementNotVisible($this->providerLink);
+        $userCheckout = $basket->addProductToBasketAndOpenUserCheckout('1000', 1);
+        $userCheckout->selectOptionNoRegistration();
+        $I->dontSeeElement($providerLinkElement);
 
         $this->setProviderState(true);
-        $basket
-            ->addProductToBasketAndOpenUserCheckout('1000', 1)
-            ->selectOptionNoRegistration();
-        $I->waitForElementNotVisible($this->providerLink);
-        $I->assertGreaterOrEquals(1, count($I->grabMultiple($this->providerLink)));
+        $basket = new Basket($I);
+        $userCheckout = $basket->addProductToBasketAndOpenUserCheckout('1000', 1);
+        $userCheckout->selectOptionNoRegistration();
+        $I->seeElement($providerLinkElement);
+        $I->assertGreaterOrEquals(1, count($I->grabMultiple($providerLinkElement)));
     }
 
     public function testProviderVisibilityOnCheckoutWithNewAccount(AcceptanceTester $I): void
     {
+        $providerLinkElement = '//div[contains(@class, "card-body")]//' . $this->providerLink;
+
         $basket = new Basket($I);
         $basket
             ->addProductToBasketAndOpenUserCheckout('1000', 1)
             ->selectOptionRegisterNewAccount();
-        $I->waitForElementNotVisible($this->providerLink);
+        $I->dontSeeElement($providerLinkElement);
 
         $this->setProviderState(true);
         $basket
             ->addProductToBasketAndOpenUserCheckout('1000', 1)
             ->selectOptionRegisterNewAccount();
-        $I->waitForElementNotVisible($this->providerLink);
-        $I->assertGreaterOrEquals(1, count($I->grabMultiple($this->providerLink)));
+        $I->seeElement($providerLinkElement);
+        $I->assertGreaterOrEquals(1, count($I->grabMultiple($providerLinkElement)));
     }
 }
