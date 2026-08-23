34a1d6a38caf1b574cf51581ddafb62fd0eae028
hkreuter <heike.reuter@oxid-esales.com>
2026-08-23 14:51:58 +0200

    OXDEV-10136 Spike password reuse prevention + change-notification email

--- metadata.php
+++ metadata.php
@@ -11,6 +11,7 @@
 
 use OxidEsales\SecurityModule\Authentication\TwoFactorAuth\Settings\TwoFAShopSettings as TwoFactorAuthModuleSettings;
 use OxidEsales\SecurityModule\PasswordPolicy\Service\ModuleSettingsService as PasswordPolicyModuleSettings;
+use OxidEsales\SecurityModule\PasswordReuse\Service\ModuleSettingsService as PasswordReuseModuleSettings;
 use OxidEsales\SecurityModule\Captcha\Service\ModuleSettingsService as CaptchaModuleSettings;
 use OxidEsales\SecurityModule\Core\Module;
 
@@ -27,14 +28,16 @@ $aModule = [
         'de' => 'Werkzeuge zum Schutz Ihres Shops und zur Sicherung von Kundenkonten.'
     ],
     'thumbnail'   => 'logo.png',
-    'version'     => '4.0.1',
+    'version'     => '4.1.0',
     'author'      => 'OXID eSales AG',
     'url'         => 'https://github.com/OXID-eSales/security-module',
     'email'       => 'info@oxid-esales.com',
     'extend'      => [
         \OxidEsales\Eshop\Application\Component\UserComponent::class => \OxidEsales\SecurityModule\Captcha\Shop\UserComponent::class,
         \OxidEsales\Eshop\Application\Controller\NewsletterController::class => \OxidEsales\SecurityModule\Captcha\Shop\NewsletterController::class,
+        \OxidEsales\Eshop\Application\Controller\AccountPasswordController::class => \OxidEsales\SecurityModule\Shared\Controller\AccountPasswordController::class,
         \OxidEsales\Eshop\Application\Controller\ForgotPasswordController::class => \OxidEsales\SecurityModule\Shared\Controller\ForgotPasswordController::class,
+        \OxidEsales\Eshop\Application\Controller\Admin\UserMain::class => \OxidEsales\SecurityModule\Shared\Controller\Admin\UserMain::class,
         \OxidEsales\Eshop\Application\Model\User::class => \OxidEsales\SecurityModule\Shared\Model\User::class,
         \OxidEsales\Eshop\Core\InputValidator::class    => \OxidEsales\SecurityModule\Shared\Core\InputValidator::class,
         \OxidEsales\Eshop\Core\ViewConfig::class        => \OxidEsales\SecurityModule\Shared\Core\ViewConfig::class
@@ -141,6 +144,34 @@ $aModule = [
             'name'  => TwoFactorAuthModuleSettings::OTP_CODE_LIFETIME,
             'type'  => 'num',
             'value' => 300
+        ],
+
+        //Password reuse prevention + change notification (opt-in, off by default)
+        [
+            'group' => 'password_reuse',
+            'name'  => PasswordReuseModuleSettings::REUSE_PREVENTION_ENABLE,
+            'type'  => 'bool',
+            'value' => false
+        ],
+        [
+            'group' => 'password_reuse',
+            'name'  => PasswordReuseModuleSettings::CHANGE_NOTIFICATION_ENABLE,
+            'type'  => 'bool',
+            'value' => false
+        ],
+        [
+            'group' => 'password_reuse',
+            'name'  => PasswordReuseModuleSettings::CUSTOMER_COLLECTION_SIZE,
+            'type'  => 'select',
+            'constraints' => '3|5|10',
+            'value' => '5'
+        ],
+        [
+            'group' => 'password_reuse',
+            'name'  => PasswordReuseModuleSettings::ADMIN_COLLECTION_SIZE,
+            'type'  => 'select',
+            'constraints' => '5|10|24',
+            'value' => '10'
         ],
     ],
 ];
